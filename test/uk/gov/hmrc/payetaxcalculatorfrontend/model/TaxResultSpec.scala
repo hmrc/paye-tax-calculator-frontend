0ba15f13332e6b3a6707e40d4c5ab198933eb030
Yuan Zhang <yuan.zhang@digital.hmrc.gov.uk>
2017-02-03 11:48:31 +0000

    Payec 7 follow master (#10)
    * PAYEC-7-about-tax-code:verify the tax code, but no error message yet
    
    * PAYEC-7-about-tax-code:verify the tax code, with error message
    
    * PAYEC-7-about-tax-code:verify the tax code, with error message
    
    * PAYEC-7:verify the tax code, with error message, linking to next page(over65 and salary), the list about you_have_told_us_items in htmls are working
    
    * PAYEC-7: refactored follow git commemts
    
    * PAYEC-7: fix some bugs, and auto focus on user seletion
    
    * PAYEC-7: fix some bugs, and auto focus on user seletion, and tests passed
    
    * PAYEC-7: refactored and units tests added
    
    * PAYEC-7: refactored with more and better messages
    
    * PAYEC-7: refactored with more and better messages
    
    * added result page
    
    * separate grossPay and total deductions. Set basic layout for results page.
    
    * change submit functions to redirect calls to show correct url for each page
    
    * PAYEC-7: redirect and and salary to list
    
    * PAYEC-7: use badRequest and globalError rather than redirect and flash, and spelling error fixed
    
    * added calculation function and have results shown on the results page.
    
    * refactor formatting and added tests
    
    * added missing tests
    
    * PAYEC-7: result page looks the same as the prototype
    
    * corrected spelling mistake
    
    * PAYEC-7: merged from puals branch
    
    * PAYEC-7: messages add, reformmat the indents, and add tabs label, pauls added tests all passed
    
    * changed to function to get current tax year
    
    * PAYEC-7: s/summery/summary/g, tab lables are in messages, spacce between main div and you_have_told_us
    
    * refactor tests, change extraction functions Over65 and Salary None cases to throw exceptions and change tests cases to match

---
+++ test/uk/gov/hmrc/payetaxcalculatorfrontend/model/TaxResultSpec.scala
@@ -0,0 +1,122 @@
+/*
+ * Copyright 2017 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.payetaxcalculatorfrontend.model
+
+import uk.gov.hmrc.play.test.UnitSpec
+import uk.gov.hmrc.payetaxcalculatorfrontend.model.TaxResult._
+
+class TaxResultSpec extends UnitSpec {
+
+  "Extracting Tax Code from user response" should {
+
+    "return tax code that a user has provided" in {
+      extractTaxCode(QuickCalcAggregateInput(Some(UserTaxCode(true,Some("K452"))), None, None)) shouldBe "K452"
+    }
+
+    "return default tax code: 1150L if a user does not provide one" in {
+      extractTaxCode(QuickCalcAggregateInput(Some(UserTaxCode(false,None)), None, None)) shouldBe "1150L"
+    }
+  }
+
+  "Extracting Over65 answer from user response" should {
+
+    "return true if the response is user is over 65" in {
+      extractOver65(QuickCalcAggregateInput(None, Some(Over65(true)), None)) shouldBe "true"
+    }
+
+    "return false if the reponse is user is not over 65" in {
+      extractOver65(QuickCalcAggregateInput(None, Some(Over65(false)), None)) shouldBe "false"
+    }
+
+    "return an error with message with \"No answer has been provided for the question: Are you Over 65?\" if no response" in {
+      val thrown = intercept[Exception]{
+        extractOver65(QuickCalcAggregateInput(None, None, None))
+      }
+      thrown.getMessage shouldBe "No answer has been provided for the question: Are you Over 65?"
+    }
+  }
+
+  "Extracting Salary from user response" should {
+
+    "return if response provided is the Yearly Salary" in {
+      extractSalary(QuickCalcAggregateInput(None, None, Some(Yearly(20000)))) shouldBe 20000
+    }
+
+    "return if response provided is the Monthly Salary" in {
+      extractSalary(QuickCalcAggregateInput(None, None, Some(Monthly(2000)))) shouldBe 2000
+    }
+
+    "return if response provided is the Weekly Salary" in {
+      extractSalary(QuickCalcAggregateInput(None, None, Some(Weekly(200)))) shouldBe 200
+    }
+
+    "return if response provided is the Daily Salary" in {
+      extractSalary(QuickCalcAggregateInput(None, None, Some(Daily(20,0)))) shouldBe 20
+    }
+
+    "return if response provided is the Hourly Salary" in {
+      extractSalary(QuickCalcAggregateInput(None, None, Some(Hourly(2, 0)))) shouldBe 2
+    }
+
+    "return an error with message \"No Salary has been provided\" if no response" in {
+      val thrown = intercept[Exception]{
+        extractSalary(QuickCalcAggregateInput(None, None, None))
+      }
+      thrown.getMessage shouldBe "No Salary has been provided."
+
+    }
+  }
+
+  "Extracting Pay Period from user response" should {
+
+    "return  if response provided is Yearly" in {
+      extractPayPeriod(QuickCalcAggregateInput(None, None, Some(Yearly(0)))) shouldBe "annual"
+    }
+
+    "return if response provided is Monthly" in {
+      extractPayPeriod(QuickCalcAggregateInput(None, None, Some(Monthly(0)))) shouldBe "monthly"
+    }
+
+    "return if response provided is Weekly" in {
+      extractPayPeriod(QuickCalcAggregateInput(None, None, Some(Weekly(0)))) shouldBe "weekly"
+    }
+
+    "return empty string if response is Daily" in {
+      extractPayPeriod(QuickCalcAggregateInput(None, None, Some(Daily(0,0)))) shouldBe ""
+    }
+
+    "return empty string if response is Hourly" in {
+      extractPayPeriod(QuickCalcAggregateInput(None, None, Some(Hourly(0,0)))) shouldBe ""
+    }
+  }
+
+  "Extracting Hours from user response" should {
+
+    "return if response is hours in Daily" in {
+      extractHours(QuickCalcAggregateInput(None, None, Some(Daily(0,40)))) shouldBe 40
+    }
+
+    "return if response is hours in Hourly" in {
+      extractHours(QuickCalcAggregateInput(None, None, Some(Hourly(0,20)))) shouldBe 20
+    }
+
+    "return if response is not Daily or Hourly" in {
+      extractHours(QuickCalcAggregateInput(None, None, Some(Weekly(0)))) shouldBe -1
+    }
+  }
+
+}
