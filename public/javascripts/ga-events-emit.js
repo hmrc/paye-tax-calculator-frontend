7d11b88848de8a8c3db860cbd3249affe00c04bd
Waldemar Wosiński <kastoestoramadus@gmail.com>
2017-04-11 13:42:59 +0100

    GA print tracking added

--- public/javascripts/ga-events-emit.js
+++ public/javascripts/ga-events-emit.js
@@ -1,18 +1,17 @@
 $(function() {
-// TODO conditional helpSend
-// TODO welsh switch after the page was loaded
-// FIXME remove the logging
     $("a.welshSwitch").on("mouseup", function () {
         ga("send", "event", "welsh-switch-clicked", this.getAttribute("href"));
     });
 
     $("#get-help-action").on("mouseup", function () {
-        console.log(this.getAttribute("href"));
         ga("send", "event", "help-switch-clicked", window.location.pathname);
     });
 
     $("#report-submit").on("mouseup", function () {
-        console.log(["send", "event", "help-send-clicked", window.location.pathname]);
         ga("send", "event", "help-send-clicked", window.location.pathname);
+    });
+    //might be not needed if the page navigation covers this
+    $("#print-calculation").on("mouseup", function () {
+        ga("send", "event", "print-calculation-clicked", this.getAttribute("href"));
     });
 });
