6c6dd1d3bcca652e0ff7ca7cf5dc46e5c6505da7
Richard Dennehy <richard.dennehy@digital.hmrc.gov.uk>
2018-03-12 11:22:45 +0000

    updated licence header

--- app/uk/gov/hmrc/payetaxcalculatorfrontend/config/CSRFBypassFilter.scala
+++ app/uk/gov/hmrc/payetaxcalculatorfrontend/config/CSRFBypassFilter.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 HM Revenue & Customs
+ * Copyright 2018 HM Revenue & Customs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -34,5 +34,3 @@ object CSRFBypassFilter extends Filter with MicroserviceFilterSupport {
       rh.copy(headers = rh.headers.add("Csrf-Token" -> "nocheck"))
 
 }
-
-
--- app/uk/gov/hmrc/payetaxcalculatorfrontend/quickmodel/QuickCalcAggregateInput.scala
+++ app/uk/gov/hmrc/payetaxcalculatorfrontend/quickmodel/QuickCalcAggregateInput.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 HM Revenue & Customs
+ * Copyright 2018 HM Revenue & Customs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -48,5 +48,3 @@ object QuickCalcAggregateInput {
   def newInstance = QuickCalcAggregateInput(None, None, None, None, None)
   implicit val format = Json.format[QuickCalcAggregateInput]
 }
-
-
