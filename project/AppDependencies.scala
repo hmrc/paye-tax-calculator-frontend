8f17c9b01a4970bcc0eb9545b906842b00a7eb50
Martin Rolfe <52818407+mrolfe88@users.noreply.github.com>
2020-08-28 16:21:46 +0100

    [HMA-3459] Use govuk component for timeout

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -16,8 +16,8 @@ object AppDependencies {
   private val taxYearVersion                    = "1.1.0"
   private val taxKalcVersion                    = "0.10.0"
   private val catsCoreVersion                   = "2.1.0"
-  private val govUkTemplate                     = "0.44.0-play-26"
-  private val hmrcFrontend                      = "0.17.0-play-26"
+  private val govUkTemplate                     = "0.50.0-play-26"
+  private val hmrcFrontend                      = "0.18.0-play-26"
 
   val compile: Seq[ModuleID] = Seq(
     ws,
