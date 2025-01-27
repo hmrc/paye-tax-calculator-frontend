c88f938ded09168bd094d9310c1168f0f9d15c15
pdhiman3006 <176318726+pdhiman3006@users.noreply.github.com>
2025-01-27 11:38:04 +0000

    HMA-9589 unit test cases for pension page

--- test/controllers/PensionsContributionFixedControllerSpec.scala
+++ test/controllers/PensionsContributionFixedControllerSpec.scala
@@ -17,8 +17,10 @@
 package controllers
 
 import forms.PensionContributionFormProvider
+import models.QuickCalcAggregateInput
 import org.apache.pekko.Done
 import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
 import org.mockito.ArgumentMatchers.any
 import org.mockito.Mockito.{times, verify, when}
 import org.scalatest.TryValues
@@ -30,7 +32,7 @@ import play.api.test.Helpers.{status, _}
 import play.api.i18n.{Messages, MessagesApi}
 import play.api.inject.bind
 import play.api.inject.guice.GuiceApplicationBuilder
-import play.api.mvc.AnyContentAsEmpty
+import play.api.mvc.{AnyContentAsEmpty, Cookie}
 import play.api.test.CSRFTokenHelper._
 import play.api.test.FakeRequest
 import services.QuickCalcCache
@@ -42,26 +44,32 @@ import views.html.pages.PensionContributionsFixedView
 import scala.concurrent.Future
 
 class PensionsContributionFixedControllerSpec
-  extends BaseSpec
+    extends BaseSpec
     with AnyWordSpecLike
-  with TryValues
-  with ScalaFutures
-  with IntegrationPatience
+    with TryValues
+    with ScalaFutures
+    with IntegrationPatience
     with MockitoSugar
-  with CSRFTestHelper {
+    with CSRFTestHelper {
 
   val formProvider = new PensionContributionFormProvider()
-  val form = formProvider()
+  val form         = formProvider()
 
   lazy val fakeRequest: FakeRequest[AnyContentAsEmpty.type] =
     FakeRequest("", "").withCSRFToken
       .asInstanceOf[FakeRequest[AnyContentAsEmpty.type]]
 
-  def messagesThing(app: Application): Messages =
-    app.injector.instanceOf[MessagesApi].preferred(fakeRequest)
+  def messagesThing(
+    app:  Application,
+    lang: String = "en"
+  ): Messages =
+    if (lang == "cy")
+      app.injector.instanceOf[MessagesApi].preferred(fakeRequest.withCookies(Cookie("PLAY_LANG", "cy")))
+    else
+      app.injector.instanceOf[MessagesApi].preferred(fakeRequest)
 
   "Show Pension Contributions Form" should {
-    "return 200 and an empty list of aggregate data" in {
+    "return 303 and an empty list of aggregate data" in {
 
       val mockCache = MockitoSugar.mock[QuickCalcCache]
 
@@ -87,49 +95,164 @@ class PensionsContributionFixedControllerSpec
         verify(mockCache, times(1)).fetchAndGetEntry()(any())
       }
     }
-    "return 200 and a list of current aggregate data containing Tax Code and pension and tax code answered" in {
+
+    "return 200" when {
+
+      def test200(lang: String = "en") = {
+        val mockCache = MockitoSugar.mock[QuickCalcCache]
+
+        when(mockCache.fetchAndGetEntry()(any())) thenReturn Future.successful(
+          cacheSalaryTaxCodeSavedPensionContributionsFixed
+        )
+
+        val application = new GuiceApplicationBuilder()
+          .overrides(bind[QuickCalcCache].toInstance(mockCache))
+          .build()
+
+        implicit val messages: Messages = messagesThing(application, lang)
+
+        running(application) {
+
+          val request = FakeRequest(GET, routes.PensionContributionsFixedController.showPensionContributionForm.url)
+            .withHeaders(HeaderNames.xSessionId -> "test")
+            .withCookies(Cookie("PLAY_LANG", lang))
+            .withCSRFToken
+
+          val result = route(application, request).get
+          val doc: Document = Jsoup.parse(contentAsString(result))
+          val title   = doc.select(".govuk-heading-xl").text
+          val para    = doc.select(".govuk-body").text()
+          val label   = doc.select(".govuk-label").text()
+          val hint    = doc.select(".govuk-hint").text()
+          val button  = doc.select(".govuk-button").text
+          val deskpro = doc.select(".govuk-link")
+
+          val view = application.injector.instanceOf[PensionContributionsFixedView]
+
+          val formFilled = form.fill(cacheSalaryTaxCodeSavedPensionContributionsFixed.get.savedPensionContributions.get)
+
+          status(result) mustEqual OK
+          title must include(messages("quick_calc.you_have_told_us.about_pension_contributions.label"))
+          para  must include(messages("quick_calc.pensionContributionsFixed.subheading"))
+          label mustEqual (messages("quick_calc.pensionContributionsFixed.input.heading"))
+          hint mustEqual (messages("quick_calc.pensionContributionsFixed.hint"))
+          para must include(messages("quick_calc.pensionContributionsFixed.link"))
+          button mustEqual (messages("continue"))
+          if (lang == "cy")
+            deskpro.text()    must include("A yw’r dudalen hon yn gweithio’n iawn? (yn agor tab newydd)")
+          else deskpro.text() must include("Is this page not working properly? (opens in new tab)")
+
+          removeCSRFTagValue(contentAsString(result)) mustEqual removeCSRFTagValue(
+            view(formFilled, cacheSalaryStatePensionTaxCode.get.additionalQuestionItems()(messages, mockAppConfig))(
+              request,
+              messagesThing(application, lang)
+            ).toString
+          )
+          verify(mockCache, times(1)).fetchAndGetEntry()(any())
+        }
+
+      }
+      "a list of current aggregate data containing Tax Code and pension and tax code answered in English" in {
+        test200()
+      }
+      "a list of current aggregate data containing Tax Code and pension and tax code answered in Welsh" in {
+        test200("cy")
+      }
+    }
+
+  }
+
+  "Submit Pension Contributions Form" should {
+
+    def test400(
+      fetchResponse: Option[QuickCalcAggregateInput],
+      pensionAmount: String,
+      errorMessages: String = "quick_calc.pensionContributionError.invalidFormat",
+      lang:          String = "en"
+    ) = {
       val mockCache = MockitoSugar.mock[QuickCalcCache]
 
-      when(mockCache.fetchAndGetEntry()(any())) thenReturn Future.successful(cacheSalaryTaxCodeSavedPensionContributionsFixed)
+      when(mockCache.fetchAndGetEntry()(any())) thenReturn Future.successful(fetchResponse)
 
       val application = new GuiceApplicationBuilder()
         .overrides(bind[QuickCalcCache].toInstance(mockCache))
         .build()
-
-      implicit val messages: Messages = messagesThing(application)
-
+      implicit val messages: Messages = messagesThing(application, lang)
       running(application) {
 
-        val request = FakeRequest(GET, routes.PensionContributionsFixedController.showPensionContributionForm.url)
+        val formData = Map("gaveUsPensionPercentage" -> "false",
+                           "monthlyPensionContributions" -> pensionAmount,
+                           "yearlyContributionAmount"    -> "480")
+
+        val request = FakeRequest(POST, routes.PensionContributionsFixedController.submitPensionContribution.url)
+          .withFormUrlEncodedBody(form.bind(formData).data.toSeq: _*)
           .withHeaders(HeaderNames.xSessionId -> "test")
+          .withCookies(Cookie("PLAY_LANG", lang))
           .withCSRFToken
 
         val result = route(application, request).get
 
-        val view = application.injector.instanceOf[PensionContributionsFixedView]
+        status(result) mustEqual BAD_REQUEST
 
-        val formFilled = form.fill(cacheSalaryTaxCodeSavedPensionContributionsFixed.get.savedPensionContributions.get)
+        val parseHtml = Jsoup.parse(contentAsString(result))
 
-        status(result) mustEqual OK
+        val errorHeader      = parseHtml.getElementsByClass("govuk-error-summary__title").text()
+        val errorMessageLink = parseHtml.getElementsByClass("govuk-list govuk-error-summary__list").text()
+        val errorMessage     = parseHtml.getElementsByClass("govuk-error-message").text()
 
-        removeCSRFTagValue(contentAsString(result)) mustEqual removeCSRFTagValue(
-          view(formFilled,
-            cacheSalaryStatePensionTaxCode.get.additionalQuestionItems()(messages, mockAppConfig))(
-            request,
-            messagesThing(application)
-          ).toString
-        )
-        verify(mockCache, times(1)).fetchAndGetEntry()(any())
+        println(" error message is ::" + errorMessage)
+        println(" error messages  is ::" + messages(errorMessages))
+
+        errorHeader mustEqual messages("error.summary.title")
+        errorMessageLink.contains(messages(errorMessages)) mustEqual true
+        errorMessage.contains(messages(errorMessages)) mustEqual true
+
       }
     }
-  }
 
-  "Submit Pension Contributions Form" should {
+    "return 400" when {
+      "Form is in English" when {
+        "there is aggregate data and an error message for invalid pension amount " in {
+          test400(cacheSalaryTaxCodeSavedPensionContributionsFixed, "40;")
+        }
+        "there is aggregate data and an error message for invalid pension contribution when amount is over ten million " in {
+          test400(cacheSalaryTaxCodeSavedPensionContributionsFixed,
+                  "1099999999",
+                  "quick_calc.pensionContributionError.aboveTenMill")
+        }
+        "there is no aggregate data and and an error message for invalid pension contributions" in {
+          test400(None, "40;")
+        }
+        "there is no aggregate data and an error message for pension contributions when value is more than 2dp" in {
+          test400(None, "40.023", "quick_calc.pensionContributionError.poundAndPence")
+        }
+      }
+      "Form is in Welsh" when {
+        "there is aggregate data and an error message for invalid pension amount " in {
+          test400(cacheSalaryTaxCodeSavedPensionContributionsFixed, "40;", lang = "cy")
+        }
+        "there is aggregate data and an error message for invalid pension contributiom when amount is over ten million " in {
+          test400(cacheSalaryTaxCodeSavedPensionContributionsFixed,
+                  "1099999999",
+                  "quick_calc.pensionContributionError.aboveTenMill",
+                  lang = "cy")
+        }
+        "there is no aggregate data and and an error message for invalid pension contributions" in {
+          test400(None, "40;", lang = "cy")
+        }
+        "there is no aggregate data and an error message for pension contributions when value is more than 2dp" in {
+          test400(None, "40.023", "quick_calc.pensionContributionError.poundAndPence", "cy")
+        }
+      }
+
+    }
     "return 400, with aggregate data and an error message for invalid pension amount" in {
 
       val mockCache = MockitoSugar.mock[QuickCalcCache]
 
-      when(mockCache.fetchAndGetEntry()(any())) thenReturn Future.successful(cacheSalaryTaxCodeSavedPensionContributionsFixed)
+      when(mockCache.fetchAndGetEntry()(any())) thenReturn Future.successful(
+        cacheSalaryTaxCodeSavedPensionContributionsFixed
+      )
 
       val application = new GuiceApplicationBuilder()
         .overrides(bind[QuickCalcCache].toInstance(mockCache))
@@ -137,7 +260,9 @@ class PensionsContributionFixedControllerSpec
       implicit val messages: Messages = messagesThing(application)
       running(application) {
 
-        val formData = Map("gaveUsPensionPercentage" -> "false", "monthlyPensionContributions" -> "40;", "yearlyContributionAmount" -> "480")
+        val formData = Map("gaveUsPensionPercentage" -> "false",
+                           "monthlyPensionContributions" -> "40;",
+                           "yearlyContributionAmount"    -> "480")
 
         val request = FakeRequest(POST, routes.PensionContributionsFixedController.submitPensionContribution.url)
           .withFormUrlEncodedBody(form.bind(formData).data.toSeq: _*)
@@ -150,9 +275,9 @@ class PensionsContributionFixedControllerSpec
 
         val parseHtml = Jsoup.parse(contentAsString(result))
 
-        val errorHeader = parseHtml.getElementsByClass("govuk-error-summary__title").text()
+        val errorHeader      = parseHtml.getElementsByClass("govuk-error-summary__title").text()
         val errorMessageLink = parseHtml.getElementsByClass("govuk-list govuk-error-summary__list").text()
-        val errorMessage = parseHtml.getElementsByClass("govuk-error-message").text()
+        val errorMessage     = parseHtml.getElementsByClass("govuk-error-message").text()
 
         errorHeader mustEqual "There is a problem"
         errorMessageLink.contains(expectedInvalidPensionsErrorMessage) mustEqual true
@@ -165,7 +290,9 @@ class PensionsContributionFixedControllerSpec
 
       val mockCache = MockitoSugar.mock[QuickCalcCache]
 
-      when(mockCache.fetchAndGetEntry()(any())) thenReturn Future.successful(cacheSalaryTaxCodeSavedPensionContributionsFixed)
+      when(mockCache.fetchAndGetEntry()(any())) thenReturn Future.successful(
+        cacheSalaryTaxCodeSavedPensionContributionsFixed
+      )
 
       val application = new GuiceApplicationBuilder()
         .overrides(bind[QuickCalcCache].toInstance(mockCache))
@@ -173,7 +300,9 @@ class PensionsContributionFixedControllerSpec
       implicit val messages: Messages = messagesThing(application)
       running(application) {
 
-        val formData = Map("gaveUsPensionPercentage" -> "false", "monthlyPensionContributions" -> "1099999999", "yearlyContributionAmount" -> "480")
+        val formData = Map("gaveUsPensionPercentage" -> "false",
+                           "monthlyPensionContributions" -> "1099999999",
+                           "yearlyContributionAmount"    -> "480")
 
         val request = FakeRequest(POST, routes.PensionContributionsFixedController.submitPensionContribution.url)
           .withFormUrlEncodedBody(form.bind(formData).data.toSeq: _*)
@@ -186,9 +315,9 @@ class PensionsContributionFixedControllerSpec
 
         val parseHtml = Jsoup.parse(contentAsString(result))
 
-        val errorHeader = parseHtml.getElementsByClass("govuk-error-summary__title").text()
+        val errorHeader      = parseHtml.getElementsByClass("govuk-error-summary__title").text()
         val errorMessageLink = parseHtml.getElementsByClass("govuk-list govuk-error-summary__list").text()
-        val errorMessage = parseHtml.getElementsByClass("govuk-error-message").text()
+        val errorMessage     = parseHtml.getElementsByClass("govuk-error-message").text()
 
         errorHeader mustEqual "There is a problem"
         errorMessageLink.contains(expectedInvalidPensionMessageOverTenMill) mustEqual true
@@ -208,7 +337,9 @@ class PensionsContributionFixedControllerSpec
       implicit val messages: Messages = messagesThing(application)
       running(application) {
 
-        val formData = Map("gaveUsPensionPercentage" -> "false", "monthlyPensionContributions" -> "40;", "yearlyContributionAmount" -> "480")
+        val formData = Map("gaveUsPensionPercentage" -> "false",
+                           "monthlyPensionContributions" -> "40;",
+                           "yearlyContributionAmount"    -> "480")
 
         val request = FakeRequest(POST, routes.PensionContributionsFixedController.submitPensionContribution.url)
           .withFormUrlEncodedBody(form.bind(formData).data.toSeq: _*)
@@ -242,7 +373,9 @@ class PensionsContributionFixedControllerSpec
       implicit val messages: Messages = messagesThing(application)
       running(application) {
 
-        val formData = Map("gaveUsPensionPercentage" -> "false", "monthlyPensionContributions" -> "40.023", "yearlyContributionAmount" -> "480")
+        val formData = Map("gaveUsPensionPercentage" -> "false",
+                           "monthlyPensionContributions" -> "40.023",
+                           "yearlyContributionAmount"    -> "480")
 
         val request = FakeRequest(POST, routes.PensionContributionsFixedController.submitPensionContribution.url)
           .withFormUrlEncodedBody(form.bind(formData).data.toSeq: _*)
