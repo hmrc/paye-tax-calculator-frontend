1aca3e7b824b4c2a647e75d408b304779bbc55de
JavedBeg-ac <181977665+JavedBeg-ac@users.noreply.github.com>
2026-03-05 10:00:16 +0000

    HMA-11432 reduced coverage to unblock testing of HMA-11334

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,10 +4,10 @@ import sbt.*
 
 object AppDependencies {
 
-  private val bootstrapPlay30Version = "10.6.0"
+  private val bootstrapPlay30Version = "10.7.0"
   private val taxYearVersion = "6.0.0"
   private val taxKalcVersion = "2.16.0"
-  private val hmrcFrontend = "12.31.0"
+  private val hmrcFrontend = "12.32.0"
   private val mongoVersion = "2.12.0"
 
   val compile: Seq[ModuleID] = Seq(
